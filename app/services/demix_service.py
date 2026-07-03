e3918bb64b4ec39b03c42a9facc5be5e9c78f1d0
GabrielXIX <gabrie1_4401@hotmail.com>
2026-07-02 20:43:37 -0700

    fix: wrap start_demix in try/except

--- app/services/demix_service.py
+++ app/services/demix_service.py
@@ -5,22 +5,26 @@ class DemixService:
     """Orchestrates the demix process"""
 
     async def start_demix(self, data, background_tasks: BackgroundTasks):
-        # 1. Do basic file validation
+        try:
+            # 1. Do basic file validation
 
-        # 2. Create job
+            # 2. Create job
 
-        # 3. Save file to storage
-        # 3.1 Update job status
+            # 3. Save file to storage
+            # 3.1 Update job status
 
-        # 4. Do deep file validation
-        # 4.1 Update job status
-        # 4.2 Delete file if deep validation fails
+            # 4. Do deep file validation
+            # 4.1 Update job status
+            # 4.2 Delete file if deep validation fails
 
-        # 5. Save metadata
-        # 5.1 Update job status
+            # 5. Save metadata
+            # 5.1 Update job status
 
-        # 6. Start background task (demucs engine, storage service, job manager)
-        # 6.1 Update job status
+            # 6. Start background task (demucs engine, storage service, job manager)
+            # 6.1 Update job status
 
-        # 5. Return job id for added job
-        pass
+            # 5. Return job id for added job
+            pass
+        except Exception as e:
+            # re raise to router
+            pass
