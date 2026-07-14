bf0723d3126014c35aa4eebae85411a67ac93348
GabrielXIX <gabrie1_4401@hotmail.com>
2026-07-13 18:58:40 -0700

    feat: add initial lifespan method skeleton

--- app/main.py
+++ app/main.py
@@ -1,6 +1,25 @@
+from contextlib import asynccontextmanager
+
 from api.routers.demix import router as demix_router
 from fastapi import FastAPI
 
-app = FastAPI(title="Source Demixing System", version="", description="")
+from app.core.logger import logger
 
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    logger.info("Application starting up...")
+    # Read settings
+    # Do sanity check
+    # Set timeout for startup operations
+    try:
+        yield
+    finally:
+        # set timeout for shutdown operations
+        logger.info("Application shutting down...")
+
+
+app = FastAPI(
+    title="Source Demixing System", version="", description="", lifespan=lifespan
+)
 app.include_router(demix_router, prefix="v1")
