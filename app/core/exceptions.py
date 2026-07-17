426a9822ca0c5d9525f5d48c10b751770cce412e
GabrielXIX <gabrie1_4401@hotmail.com>
2026-07-16 20:52:45 -0700

    feat: add job not found exception

--- app/core/exceptions.py
+++ app/core/exceptions.py
@@ -4,3 +4,11 @@ class SDSException(Exception):
 
 class InvalidAudioError(SDSException):
     pass
+
+
+class JobNotFoundError(SDSException):
+    pass
+
+
+# Python built in errors used in the project:
+# KeyError, TypeError
